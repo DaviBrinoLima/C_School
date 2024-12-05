0c9b89ed63592276e2c30a86ff6c6139deba5283
DaviBrinoLima <davibrinolima@furg.br>
2024-12-05 09:41:39 -0300

    Exec e conteudo aula 29/11

---
+++ Strings/input_string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main() {
+    char s[10];
+
+
+    printf("Insira a string: ");
+    scanf("%9s", s);
+
+
+    printf("%s \n", s);
+    return 0;
+}
---
+++ Vetores/Exec_1_vetores.c
@@ -0,0 +1,29 @@
+// Faça um programa que defina um vetor v= {4,1,5,8,6}, imprimindo o menor e o maior valor presente
+
+
+#include <stdio.h>
+
+
+int main() {
+    int v[5] = {4,1,5,8,6};
+    int len_v = 5;
+
+
+    int maior = v[0]; 
+    int menor = v[0];
+
+
+    for (int i = 0; i<len_v; i += 1) {
+        if (v[i] < menor) {
+            menor = v[i];
+        }
+
+        if (v[i] > maior) {
+            maior = v[i];
+        }
+    }
+
+
+    printf("Menor: %d \nMaior: %d \n", menor,maior);
+    return 0;
+}
